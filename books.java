eca4750302337db63b6079df0ed633cb213c9573
Happy <manncovey@gmail.com>
2024-10-04 17:48:45 -0600

    Add files via upload

---
+++ books.java
@@ -0,0 +1,11 @@
+/** Madison Covey
+ * Week 7
+ * Mr.Gross Class
+ */
+public class books {
+ public static void main(String[] args) {
+    String [] book= {"hardcover" , "Softcover" , "Digital" , "Pop-up"};
+    System.out.println(book [2]);
+ }
+    
+}
